d020cbf1876b3401f3a7c1b8cc3b9f754eb2ca51
Osmar ICancino <o.cancinodiaz@gmail.com>
2019-09-18 00:11:32 -0500

    Initial commit

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "42f6037782ffa6d0229e74706d3206de";
+const String TMDB_BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
